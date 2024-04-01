1c9b464ac5ba97c6e92c834534b6dfa615819ac6
Kao <chingching.kao11@gmail.com>
2024-03-31 22:41:31 -0700

    updated the basepath

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,11 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    basePath: isProd ? '/ching_portfolio': '',
+    basePath: isProd ? '/chingching11.github.io/ching_portfolio': '',
     output: 'export',
     reactStrictMode: true,
     assetPrefix: '/ching_portfolio',
+    images: { unoptimized: true }
     
 };
 
